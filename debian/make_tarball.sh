ddba60f939fa664c1c1aeeb8eec135321977bea9
Adrian Knoth <adi@drcomp.erfurt.thur.de>
2012-05-29 21:56:14 +0200

    Update helper script to replace waf by waf-light
    Also automate tarball generation

--- debian/make_tarball.sh
+++ debian/make_tarball.sh
@@ -26,6 +26,22 @@ git archive master | tar -C "${TARGET_DIR}" -xf -
 echo "Cleaning git files from export directory"
 find "${TARGET_DIR}" -name ".git*" -delete
 
+# replace waf by waf-light
+echo "Replacying waf by waf-light"
+( cd "${TARGET_DIR}" && \
+    ./waf ; \
+    mv .waf*/waflib waflib && \
+    rm waf ; \
+    find ./waflib -iname "*.pyc" -delete ; \
+    wget http://waf.googlecode.com/git-history/waf-1.6.11/waf-light ; \
+    ln -s waf-light waf && chmod +x waf-light
+)
+
 # make it DFSG clean
 echo "Making it DFSG clean"
 rm -rf "${TARGET_DIR}/windows" "${TARGET_DIR}/macosx"
+
+# create final tarball
+echo "Creating tarball"
+tar -C "${TARGET_DIR}/../" -cvzf "${TARGET_DIR}".tar.gz \
+    "`basename "${TARGET_DIR}"`" && echo "All done. Enjoy."
